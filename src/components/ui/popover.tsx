f6ad3b74739dbff3bb265c35561211fef9aaff4f
oscargabellini <oscargabellini@gmail.com>
2025-10-01 23:09:04 +0200

    chore: remove console log from popover content

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -26,9 +26,6 @@ function PopoverContent({
         data-slot="popover-content"
         align={align}
         sideOffset={sideOffset}
-        onMouseEnter={(e) => {
-          console.log("popover content mouse entered");
-        }}
         className={cn(
           "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-auto min-w-[320px] origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden",
           className
